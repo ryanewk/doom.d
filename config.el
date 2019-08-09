afe3fa3944ce114f446088413869af605645c33b
ryane <ryanesc@gmail.com>
2019-08-09 14:26:26 -0400

    configure org mode and deft

--- config.el
+++ config.el
@@ -5,9 +5,26 @@
 
 (setq doom-font (font-spec :family "Menlo" :size 13))
 
-(add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\|org\\.txt\\)$" . org-mode))
-(setq
- org-directory "~/org"
- org-agenda-file-regexp "\\`[^.].*\\.\\(org\\.txt\\|org\\)\\'")
+(after! org
+  (add-to-list
+   'auto-mode-alist '("\\.\\(org\\|org_archive\\|org\\.txt\\)$" . org-mode))
+  (setq
+   org-directory "~/org"
+   org-agenda-file-regexp "\\`[^.].*\\.\\(org\\.txt\\|org\\)\\'")
 
-(setq deft-directory "~/org/notes")
+
+  (setq org-todo-keywords
+        '((sequence "TODO(t)" "PROJ(p)" "MEET(m)" "|" "DONE(d)")
+          (sequence "[ ](T)" "[-](P)" "[?](M)" "|" "[X](D)")
+          (sequence "NEXT(n)" "WAIT(w)" "HOLD(h)" "|" "ABRT(c)" "DEFR(f)"))
+        )
+
+  (setq
+   org-agenda-skip-scheduled-if-done t
+   org-agenda-skip-deadline-if-done t
+   )
+  )
+
+(after! deft
+  (setq deft-directory "~/org/notes")
+  )
